ce695319b1265eac60be97b015e5704174ae0b39
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-06-25 12:03:07 +0200

    Updated CIDER to 0.9.1

--- init.el
+++ init.el
@@ -9,7 +9,9 @@
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
